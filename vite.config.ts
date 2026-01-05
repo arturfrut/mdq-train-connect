7872b6e2cd819a5d281b994be94f14f80c195179
arturfrut <arturfruttero@gmail.com>
2026-01-05 17:08:13 -0300

    restarting fix deploy process

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-// import { componentTagger } from "lovable-tagger";
-
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   base: '/mdq-train-connect/'
-// }));
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -30,5 +9,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  base: '/mdq-train-connect/'
 })
