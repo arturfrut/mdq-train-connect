b8f2c43d977360e90a1e198349f29c034e35e1fc
arturfrut <arturfruttero@gmail.com>
2026-03-04 11:16:30 -0300

    fix: remove basename from BrowserRouter and update Vite base path for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename='/mdq-train-connect'>
+        <BrowserRouter>
           <Routes>
             <Route path='/' element={<Index />} />
             <Route path='*' element={<NotFound />} />
